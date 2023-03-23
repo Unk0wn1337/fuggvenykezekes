27fd864c8dff0e3365c305938b807c196a36fdae
Unk0wn1337 <116260492+Unk0wn1337@users.noreply.github.com>
2023-03-23 12:19:29 +0100

    javitott, nem error-os verzio

--- adat.js
+++ adat.js
@@ -1,11 +1,10 @@
 export const SZAMLISTA = [23,5,55,11,121];
-export const SZOVEGLISTA = ["béka","alma","répa","paradicsom"];
-export const OBJEKTUMLISTA = [
-    {nev: "Dézi", fajta: "keverék", kor:2},
-    {nev: "Bodri", fajta: "mudi", kor:12},
-    {nev: "Loki", fajta: "tacsko", kor:5},
-    {nev: "Balint", fajta: "foxterrier", kor:4},
-    {nev: "Happy", fajta: "nemetJuhasz", kor:42},
-];
-
+export const SZOVEGLISTA = ["béka", "alma", "paradicsom", "körte"];
+export const OBJEKTUMLISTA =[
+{ nev: "Dézi", fajta: "keverék", kor:2},
+{ nev: "Bodri", fajta: "mudi", kor:13},
+{ nev: "Loki", fajta: "tacskó", kor:5},
+{ nev: "Balint", fajta: "foxterrier", kor:4},
+{ nev: "Happy", fajta: "németjuhász", kor:42}
+]
 
--- rendezes.js
+++ rendezes.js
@@ -1,44 +1,29 @@
-
-export function rendezesSzoveg(lista) {
-  lista.sort();
+export function rendezesSzoveg(lista){
+  lista.sort()
 }
-export function rendezesSzam(lista) {
-  lista.sort(function (a, b) {
-    console.log(a, b, a - b);
-    return a - b;
-  });
-  function veletlenSorrend(lista) {
-    lista.sort(function (x, y) {
-      return Math.random() - 0, 5;
-    });
-  }
-  function rendezesKorSzerint(lista,kulcs) {
-    lista.sort(function (a, b) {
-      console.log(a, b);
-      return (a[kulcs] - b[kulcs]);
-    });
-  }
-  function rendezesSzovegSzerint(lista, kulcs) {
-    lista.sort(function (a, b) {
-      if (a[kulcs] > b[kulcs]) {
-        return 1;
-      } else {
-        return -1;
-      }
-    });
-    function rendezesObjektum(lista, kulcs){
-        if(typeof kulcs == "number"){
-            rendezesSzam(lista, kulcs);
-        } else {
-            rendezesSzoveg(lista, kulcs);
-        }
-    }
-
-
+export function rendezesSzam(lista){
+  lista.sort(function(a, b){return a - b});
 }
-
-
-
-
-
+export function veletlenSorrend(lista){
+  lista.sort(function(x,y){
+      return Math.random() -0.5;
+  })
+}
+function rendezesSzamSzerint(lista,kulcs){
+  lista.sort(function(a, b){
+      return a[kulcs] - b[kulcs]});
+}
+function rendezesSzovegSzerint(lista,kulcs){
+  lista.sort(function(x, y){
+      if (x[kulcs] > y[kulcs]) {return 1;}
+      if (x[kulcs] < y[kulcs]) {return -1;}
+      return 0;
+    });
+}
+export function rendezesObjektum(lista,kulcs){
+  if(typeof kulcs =='number'){
+      rendezesSzamSzerint(lista,kulcs);
+  }else{
+      rendezesSzovegSzerint(lista,kulcs);
+  }
 }
